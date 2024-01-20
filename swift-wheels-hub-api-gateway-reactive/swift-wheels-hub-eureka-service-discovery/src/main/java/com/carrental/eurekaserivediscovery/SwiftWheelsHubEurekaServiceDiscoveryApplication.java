87132ee0c50e58645cf7a7ffa31d996b285ddc67
George Niculae <george.niculae20@yahoo.com>
2024-01-20 21:02:24 +0200

    Update

--- swift-wheels-hub-api-gateway-reactive/swift-wheels-hub-eureka-service-discovery/src/main/java/com/carrental/eurekaserivediscovery/SwiftWheelsHubEurekaServiceDiscoveryApplication.java
+++ swift-wheels-hub-api-gateway-reactive/swift-wheels-hub-eureka-service-discovery/src/main/java/com/carrental/eurekaserivediscovery/SwiftWheelsHubEurekaServiceDiscoveryApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class CarRentalEurekaServiceDiscoveryApplication {
+public class SwiftWheelsHubEurekaServiceDiscoveryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CarRentalEurekaServiceDiscoveryApplication.class, args);
+        SpringApplication.run(SwiftWheelsHubEurekaServiceDiscoveryApplication.class, args);
     }
 
 }
