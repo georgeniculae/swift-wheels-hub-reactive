3dd13cba4fa6e5711e361f950bec1dabddddb520
George <george.niculae20@yahoo.com>
2024-02-16 19:53:14 +0200

    Update

--- swift-wheels-hub-agency-reactive/src/main/java/com/swiftwheelshub/agency/migration/DatabaseCollectionCreator.java
+++ swift-wheels-hub-agency-reactive/src/main/java/com/swiftwheelshub/agency/migration/DatabaseCollectionCreator.java
@@ -8,6 +8,7 @@ import com.swiftwheelshub.model.Employee;
 import com.swiftwheelshub.model.RentalOffice;
 import org.bson.types.ObjectId;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class DatabaseCollectionCreator {
@@ -28,8 +29,8 @@ public class DatabaseCollectionCreator {
 
     public static List<Car> getCars() {
         return List.of(
-                createCar("65072052d5d4531e66a0c00c", "Volkswagen", "Golf", BodyType.HATCHBACK, 2010, "black", 232000, 500.0, createBranch("65072051d5d4531e66a0c00a", "Branch 1", "Ploiesti", createRentalOffice("65072050d5d4531e66a0c008", "Rental Office 1", "contact address 1", "logo 1")), createBranch("65072051d5d4531e66a0c00a", "Branch 1", "Ploiesti", createRentalOffice("65072050d5d4531e66a0c008", "Rental Office 1", "contact address 1", "logo 1")), "url1"),
-                createCar("65072052d5d4531e66a0c00d", "Audi", "A4", BodyType.SEDAN, 2015, "white", 187000, 800.0, createBranch("65072051d5d4531e66a0c00b", "Branch 2", "Bucuresti", createRentalOffice("65072050d5d4531e66a0c009", "Rental Office 2", "contact address 2", "logo 2")), createBranch("65072051d5d4531e66a0c00b", "Branch 2", "Bucuresti", createRentalOffice("65072050d5d4531e66a0c009", "Rental Office 2", "contact address 2", "logo 2")), "url2")
+                createCar("65072052d5d4531e66a0c00c", "Volkswagen", "Golf", BodyType.HATCHBACK, 2010, "black", 232000, BigDecimal.valueOf(500), createBranch("65072051d5d4531e66a0c00a", "Branch 1", "Ploiesti", createRentalOffice("65072050d5d4531e66a0c008", "Rental Office 1", "contact address 1", "logo 1")), createBranch("65072051d5d4531e66a0c00a", "Branch 1", "Ploiesti", createRentalOffice("65072050d5d4531e66a0c008", "Rental Office 1", "contact address 1", "logo 1")), "url1"),
+                createCar("65072052d5d4531e66a0c00d", "Audi", "A4", BodyType.SEDAN, 2015, "white", 187000, BigDecimal.valueOf(800), createBranch("65072051d5d4531e66a0c00b", "Branch 2", "Bucuresti", createRentalOffice("65072050d5d4531e66a0c009", "Rental Office 2", "contact address 2", "logo 2")), createBranch("65072051d5d4531e66a0c00b", "Branch 2", "Bucuresti", createRentalOffice("65072050d5d4531e66a0c009", "Rental Office 2", "contact address 2", "logo 2")), "url2")
         );
     }
 
@@ -43,7 +44,7 @@ public class DatabaseCollectionCreator {
     }
 
     private static Car createCar(String id, String make, String model, BodyType bodyType, int yearOfProduction, String color,
-                                 int mileage, Double amount, Branch originalBranch, Branch actualBranch, String urlOfImage) {
+                                 int mileage, BigDecimal amount, Branch originalBranch, Branch actualBranch, String urlOfImage) {
         return Car.builder()
                 .id(new ObjectId(id))
                 .make(make)
