6a91ddff26f42bb12c065dee2e2ef6a710b349b8
George <george.niculae20@yahoo.com>
2024-02-16 10:30:39 +0200

    Update

--- swift-wheels-hub-email-notification-reactive/src/main/java/com/swiftwheelshub/emailnotification/SwiftWheelsHubEmailNotificationApplication.java
+++ swift-wheels-hub-email-notification-reactive/src/main/java/com/swiftwheelshub/emailnotification/SwiftWheelsHubEmailNotificationApplication.java
@@ -8,7 +8,7 @@ import reactor.blockhound.BlockHound;
 public class SwiftWheelsHubEmailNotificationApplication {
 
     public static void main(String[] args) {
-        BlockHound.install(builder -> builder.allowBlockingCallsInside("io.netty.util.concurrent.FastThreadLocalRunnable", "run"));
+        BlockHound.install();
         SpringApplication.run(SwiftWheelsHubEmailNotificationApplication.class);
     }
 
--- swift-wheels-hub-request-validator-reactive/src/main/java/com/swiftwheelshub/requestvalidator/SwiftWheelsHubRequestValidatorReactiveApplication.java
+++ swift-wheels-hub-request-validator-reactive/src/main/java/com/swiftwheelshub/requestvalidator/SwiftWheelsHubRequestValidatorReactiveApplication.java
@@ -8,7 +8,7 @@ import reactor.blockhound.BlockHound;
 public class SwiftWheelsHubRequestValidatorReactiveApplication {
 
     public static void main(String[] args) {
-        BlockHound.install(builder -> builder.allowBlockingCallsInside("io.netty.util.concurrent.FastThreadLocalRunnable", "run"));
+        BlockHound.install();
         SpringApplication.run(SwiftWheelsHubRequestValidatorReactiveApplication.class, args);
     }
 
