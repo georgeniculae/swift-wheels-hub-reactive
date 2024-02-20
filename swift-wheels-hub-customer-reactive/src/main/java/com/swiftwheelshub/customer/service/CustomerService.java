8b82a4af665a92c72c39377350b78bfa64d91ce0
George <george.niculae20@yahoo.com>
2024-02-21 00:26:25 +0200

    Update

--- swift-wheels-hub-customer-reactive/src/main/java/com/swiftwheelshub/customer/service/CustomerService.java
+++ swift-wheels-hub-customer-reactive/src/main/java/com/swiftwheelshub/customer/service/CustomerService.java
@@ -4,6 +4,7 @@ import com.swiftwheelshub.dto.RegisterRequest;
 import com.swiftwheelshub.dto.RegistrationResponse;
 import com.swiftwheelshub.dto.UserInfo;
 import com.swiftwheelshub.dto.UserUpdateRequest;
+import com.swiftwheelshub.lib.exceptionhandling.SwiftWheelsHubException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -18,40 +19,73 @@ public class CustomerService {
     private final KeycloakUserService keycloakUserService;
 
     public Mono<UserInfo> findUserByUsername(String username) {
-        return Mono.fromCallable(() -> keycloakUserService.getCurrentUser(username))
-                .subscribeOn(Schedulers.boundedElastic());
+        return Mono.fromCallable(() -> keycloakUserService.findUserByUsername(username))
+                .subscribeOn(Schedulers.boundedElastic())
+                .onErrorMap(e -> {
+                    log.error("Error while getting user by username: {}", e.getMessage());
+
+                    return new SwiftWheelsHubException(e);
+                });
     }
 
     public Mono<UserInfo> getCurrentUser(String username) {
         return Mono.fromCallable(() -> keycloakUserService.getCurrentUser(username))
-                .subscribeOn(Schedulers.boundedElastic());
+                .subscribeOn(Schedulers.boundedElastic()).onErrorMap(e -> {
+                    log.error("Error while getting current user: {}", e.getMessage());
+
+                    return new SwiftWheelsHubException(e);
+                });
     }
 
     public Mono<Long> countUsers() {
         return Mono.fromCallable(keycloakUserService::countUsers)
                 .subscribeOn(Schedulers.boundedElastic())
-                .cast(Long.class);
+                .cast(Long.class).onErrorMap(e -> {
+                    log.error("Error while counting users: {}", e.getMessage());
+
+                    return new SwiftWheelsHubException(e);
+                });
     }
 
     public Mono<RegistrationResponse> registerUser(RegisterRequest request) {
         return Mono.fromCallable(() -> keycloakUserService.registerCustomer(request))
-                .subscribeOn(Schedulers.boundedElastic());
+                .subscribeOn(Schedulers.boundedElastic())
+                .onErrorMap(e -> {
+                    log.error("Error while registering user: {}", e.getMessage());
+
+                    return new SwiftWheelsHubException(e);
+                });
     }
 
     public Mono<UserInfo> updateUser(String id, UserUpdateRequest userUpdateRequest) {
         return Mono.fromCallable(() -> keycloakUserService.updateUser(id, userUpdateRequest))
-                .subscribeOn(Schedulers.boundedElastic());
+                .subscribeOn(Schedulers.boundedElastic())
+                .onErrorMap(e -> {
+                    log.error("Error while updating user: {}", e.getMessage());
+
+                    return new SwiftWheelsHubException(e);
+                });
     }
 
     public Mono<Void> deleteUserById(String id) {
         return Mono.fromRunnable(() -> keycloakUserService.deleteUserById(id))
                 .subscribeOn(Schedulers.boundedElastic())
+                .onErrorMap(e -> {
+                    log.error("Error while deleting user: {}", e.getMessage());
+
+                    return new SwiftWheelsHubException(e);
+                })
                 .then();
     }
 
     public Mono<Void> signOut(String id) {
         return Mono.fromRunnable(() -> keycloakUserService.signOut(id))
                 .subscribeOn(Schedulers.boundedElastic())
+                .onErrorMap(e -> {
+                    log.error("Error while signing out user: {}", e.getMessage());
+
+                    return new SwiftWheelsHubException(e);
+                })
                 .then();
     }
 
--- swift-wheels-hub-customer-reactive/src/test/java/com/swiftwheelshub/customer/service/CustomerServiceTest.java
+++ swift-wheels-hub-customer-reactive/src/test/java/com/swiftwheelshub/customer/service/CustomerServiceTest.java
@@ -1,9 +1,17 @@
 package com.swiftwheelshub.customer.service;
 
+import com.swiftwheelshub.customer.util.TestUtils;
+import com.swiftwheelshub.dto.UserInfo;
+import com.swiftwheelshub.exception.SwiftWheelsHubException;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.test.StepVerifier;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CustomerServiceTest {
@@ -14,6 +22,27 @@ class CustomerServiceTest {
     @Mock
     private KeycloakUserService keycloakUserService;
 
+    @Test
+    void findUserByUsernameTest_success() {
+        UserInfo userInfo =
+                TestUtils.getResourceAsJson("/data/UserInfo.json", UserInfo.class);
 
+        when(keycloakUserService.findUserByUsername(anyString())).thenReturn(userInfo);
+
+        customerService.findUserByUsername("1")
+                .as(StepVerifier::create)
+                .expectNext(userInfo)
+                .verifyComplete();
+    }
+
+    @Test
+    void findUserByUsernameTest_errorOnFindingByUsername() {
+        when(keycloakUserService.findUserByUsername(anyString())).thenThrow(new SwiftWheelsHubException(""));
+
+        customerService.findUserByUsername("1")
+                .as(StepVerifier::create)
+                .expectError()
+                .verify();
+    }
 
 }
