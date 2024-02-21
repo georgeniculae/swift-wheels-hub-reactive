769de4e482759afc2592c339e2f0eef93e750125
George <george.niculae20@yahoo.com>
2024-02-21 20:13:48 +0200

    Update

--- swift-wheels-hub-reactive-api-gateway/swift-wheels-hub-reactive-eureka-service-discovery/src/main/java/com/swiftwheelshubreactive/eurekaserivediscovery/SwiftWheelsHubReactiveEurekaServiceDiscoveryApplication.java
+++ swift-wheels-hub-reactive-api-gateway/swift-wheels-hub-reactive-eureka-service-discovery/src/main/java/com/swiftwheelshubreactive/eurekaserivediscovery/SwiftWheelsHubReactiveEurekaServiceDiscoveryApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class SwiftWheelsHubReactiveEureka {
+public class SwiftWheelsHubReactiveEurekaServiceDiscoveryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SwiftWheelsHubReactiveEureka.class, args);
+        SpringApplication.run(SwiftWheelsHubReactiveEurekaServiceDiscoveryApplication.class, args);
     }
 
 }
