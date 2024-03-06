be2b35b6b9e3881843957b94a5d9518183f96517
George <george.niculae20@yahoo.com>
2024-03-06 21:33:18 +0200

    Update

--- swift-wheels-hub-reactive-common/swift-wheels-hub-reactive-common-lib/src/main/java/com/swiftwheelshubreactive/lib/config/mongodb/MongoTransactionManagerConfig.java
+++ swift-wheels-hub-reactive-common/swift-wheels-hub-reactive-common-lib/src/main/java/com/swiftwheelshubreactive/lib/config/mongodb/MongoTransactionManagerConfig.java
@@ -1,7 +1,6 @@
 package com.swiftwheelshubreactive.lib.config.mongodb;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
