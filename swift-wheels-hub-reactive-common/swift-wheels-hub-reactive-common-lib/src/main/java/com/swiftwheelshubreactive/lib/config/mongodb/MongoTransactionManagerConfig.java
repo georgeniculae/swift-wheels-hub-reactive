bdf3f642de9e32162385223d0379be107b8cdcfe
George <george.niculae20@yahoo.com>
2024-03-06 22:23:51 +0200

    Update

--- swift-wheels-hub-reactive-common/swift-wheels-hub-reactive-common-lib/src/main/java/com/swiftwheelshubreactive/lib/config/mongodb/MongoTransactionManagerConfig.java
+++ swift-wheels-hub-reactive-common/swift-wheels-hub-reactive-common-lib/src/main/java/com/swiftwheelshubreactive/lib/config/mongodb/MongoTransactionManagerConfig.java
@@ -1,13 +1,13 @@
 package com.swiftwheelshubreactive.lib.config.mongodb;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
 import org.springframework.data.mongodb.ReactiveMongoTransactionManager;
 
 @Configuration
-@ConditionalOnBean(value = ReactiveMongoDatabaseFactory.class)
+@ConditionalOnProperty(prefix = "transactions", name = "enabled", havingValue = "true")
 public class MongoTransactionManagerConfig {
 
     @Bean
