5bbf53dfb02a474d10865a88a5e45df53693740d
GeorgeNiculae <george.niculae20@yahoo.com>
2024-03-21 17:40:28 +0200

    Update

--- swift-wheels-hub-reactive-request-validator/src/main/java/com/swiftwheelshubreactive/requestvalidator/config/RedisConfig.java
+++ swift-wheels-hub-reactive-request-validator/src/main/java/com/swiftwheelshubreactive/requestvalidator/config/RedisConfig.java
@@ -1,28 +1,54 @@
 package com.swiftwheelshubreactive.requestvalidator.config;
 
 import com.swiftwheelshubreactive.requestvalidator.model.SwaggerFile;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
-    @Bean
-    public ReactiveRedisOperations<String, SwaggerFile> redisOperations(ReactiveRedisConnectionFactory factory) {
-        Jackson2JsonRedisSerializer<SwaggerFile> serializer = new Jackson2JsonRedisSerializer<>(SwaggerFile.class);
+    @Value("${spring.redis.host}")
+    private String redisHost;
 
-        RedisSerializationContext.RedisSerializationContextBuilder<String, SwaggerFile> builder =
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfig = new RedisStandaloneConfiguration();
+
+        redisStandaloneConfig.setHostName(redisHost);
+        redisStandaloneConfig.setPort(redisPort);
+        redisStandaloneConfig.setPassword(redisPassword);
+
+        return new LettuceConnectionFactory(redisStandaloneConfig);
+    }
+
+    @Bean
+    public ReactiveRedisOperations<String, SwaggerFile> redisOperations(LettuceConnectionFactory connectionFactory) {
+        RedisSerializationContext.RedisSerializationContextBuilder<String, SwaggerFile> serializationContextBuilder =
                 RedisSerializationContext.newSerializationContext(new StringRedisSerializer());
 
-        RedisSerializationContext<String, SwaggerFile> context = builder.value(serializer).build();
+        serializationContextBuilder.key(new StringRedisSerializer());
+        serializationContextBuilder.value(new GenericToStringSerializer<>(SwaggerFile.class));
+        serializationContextBuilder.hashKey(new StringRedisSerializer());
+        serializationContextBuilder.hashValue(new GenericJackson2JsonRedisSerializer());
 
-        return new ReactiveRedisTemplate<>(factory, context);
+        RedisSerializationContext<String, SwaggerFile> serializationContext = serializationContextBuilder.build();
+
+        return new ReactiveRedisTemplate<>(connectionFactory, serializationContext);
     }
 
 }
