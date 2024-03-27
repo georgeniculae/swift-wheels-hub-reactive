6f6bea996e65bb33b8edd4be5d8d47fc4d9a6cd6
George <george.niculae20@yahoo.com>
2024-03-27 13:36:02 +0200

    Update

--- swift-wheels-hub-reactive-expense/src/main/java/com/swiftwheelshubreactive/expense/repository/InvoiceRepository.java
+++ swift-wheels-hub-reactive-expense/src/main/java/com/swiftwheelshubreactive/expense/repository/InvoiceRepository.java
@@ -2,6 +2,7 @@ package com.swiftwheelshubreactive.expense.repository;
 
 import com.swiftwheelshubreactive.model.Invoice;
 import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -10,6 +11,8 @@ public interface InvoiceRepository extends ReactiveMongoRepository<Invoice, Obje
 
     Mono<Invoice> findByBookingId(ObjectId bookingId);
 
+    @Query("""
+            { 'comments': { $regex: '(?i)?0' } }""")
     Flux<Invoice> findByComments(String comments);
 
     Flux<Invoice> findByCustomerUsername(String customerUsername);
