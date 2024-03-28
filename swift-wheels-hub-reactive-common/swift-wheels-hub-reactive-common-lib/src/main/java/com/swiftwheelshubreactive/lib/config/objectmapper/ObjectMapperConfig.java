fd187f44c925c0b6ec778431adbdc15f6efd8fff
George <george.niculae20@yahoo.com>
2024-03-28 12:19:47 +0200

    Update

--- swift-wheels-hub-reactive-common/swift-wheels-hub-reactive-common-lib/src/main/java/com/swiftwheelshubreactive/lib/config/objectmapper/ObjectMapperConfig.java
+++ swift-wheels-hub-reactive-common/swift-wheels-hub-reactive-common-lib/src/main/java/com/swiftwheelshubreactive/lib/config/objectmapper/ObjectMapperConfig.java
@@ -6,12 +6,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Configuration
 public class ObjectMapperConfig {
@@ -20,9 +22,17 @@ public class ObjectMapperConfig {
     @Primary
     public ObjectMapper objectMapper() {
         return Jackson2ObjectMapperBuilder.json()
-                .modules(new JavaTimeModule().addDeserializer(LocalDate.class, LocalDateDeserializer.INSTANCE))
-                .featuresToEnable(MapperFeature.ALLOW_COERCION_OF_SCALARS)
+                .modules(
+                        new JavaTimeModule()
+                                .addDeserializer(LocalDate.class, LocalDateDeserializer.INSTANCE)
+                                .addDeserializer(LocalDateTime.class, LocalDateTimeDeserializer.INSTANCE)
+                )
+                .featuresToEnable(
+                        MapperFeature.ALLOW_COERCION_OF_SCALARS,
+                        DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS
+                )
                 .featuresToDisable(
+                        SerializationFeature.FAIL_ON_EMPTY_BEANS,
                         SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS,
                         DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE
                 )
