3d0df114825d7aa415a5f7bfd3cba2f63f08314c
George <george.niculae20@yahoo.com>
2024-04-02 16:35:22 +0300

    Update

--- swift-wheels-hub-reactive-api-gateway/swift-wheels-hub-reactive-service-discovery/src/main/java/com/swiftwheelshubreactive/serivediscovery/config/EurekaSecurityConfig.java
+++ swift-wheels-hub-reactive-api-gateway/swift-wheels-hub-reactive-service-discovery/src/main/java/com/swiftwheelshubreactive/serivediscovery/config/EurekaSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.swiftwheelshubreactive.serivediscovery.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -11,14 +10,13 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@ConditionalOnProperty(prefix = "spring.security.basic", name = "enabled")
 public class EurekaSecurityConfig {
 
     @Bean
-    public SecurityFilterChain eurekaFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request.requestMatchers("/eureka/apps/**").permitAll()
-                                .anyRequest().authenticated())
+                        .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
