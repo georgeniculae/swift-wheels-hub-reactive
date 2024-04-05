ba3ac75d203454442e1d83c648f2faeb3baebb60
George <george.niculae20@yahoo.com>
2024-04-05 23:54:25 +0300

    Update

--- swift-wheels-hub-reactive-common/swift-wheels-hub-reactive-common-lib/src/main/java/com/swiftwheelshubreactive/lib/security/AuthenticationWebFilterConfig.java
+++ swift-wheels-hub-reactive-common/swift-wheels-hub-reactive-common-lib/src/main/java/com/swiftwheelshubreactive/lib/security/AuthenticationWebFilterConfig.java
@@ -3,10 +3,10 @@ package com.swiftwheelshubreactive.lib.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
-import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
 @RequiredArgsConstructor
 @ConditionalOnBean(name = "apiKeySecurityConfig")
 public class AuthenticationWebFilterConfig {
@@ -16,7 +16,7 @@ public class AuthenticationWebFilterConfig {
 
     @Bean
     public AuthenticationWebFilter authenticationWebFilter() {
-        final AuthenticationWebFilter authenticationWebFilter = new AuthenticationWebFilter(apiKeyAuthenticationManager);
+        AuthenticationWebFilter authenticationWebFilter = new AuthenticationWebFilter(apiKeyAuthenticationManager);
         authenticationWebFilter.setServerAuthenticationConverter(apiKeyAuthenticationConverter);
 
         return authenticationWebFilter;
