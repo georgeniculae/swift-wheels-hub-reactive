040661674e2873460b44c0dd7c6946f60068112d
George <george.niculae20@yahoo.com>
2024-04-06 23:54:43 +0300

    Update

--- swift-wheels-hub-reactive-customer/src/main/java/com/swiftwheelshubreactive/customer/swaggeroperation/SwaggerCustomerRouterOperations.java
+++ swift-wheels-hub-reactive-customer/src/main/java/com/swiftwheelshubreactive/customer/swaggeroperation/SwaggerCustomerRouterOperations.java
@@ -8,12 +8,14 @@ import com.swiftwheelshubreactive.dto.UserUpdateRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springdoc.core.annotations.RouterOperation;
 import org.springdoc.core.annotations.RouterOperations;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.lang.annotation.ElementType;
@@ -25,6 +27,35 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @RouterOperations(
         {
+                @RouterOperation(
+                        method = RequestMethod.GET,
+                        path = "/infos",
+                        beanClass = CustomerHandler.class,
+                        beanMethod = "findAllUsers",
+                        operation = @Operation(
+                                operationId = "findAllUsers",
+                                responses = {
+                                        @ApiResponse(
+                                                responseCode = "200",
+                                                description = "Successful",
+                                                content = @Content(
+                                                        array = @ArraySchema(schema = @Schema(implementation = UserInfo.class)),
+                                                        mediaType = MediaType.APPLICATION_JSON_VALUE
+                                                )
+                                        ),
+                                        @ApiResponse(
+                                                responseCode = "400",
+                                                description = "Bad Request",
+                                                content = @Content(schema = @Schema())
+                                        ),
+                                        @ApiResponse(
+                                                responseCode = "500",
+                                                description = "Internal Server Error",
+                                                content = @Content(schema = @Schema())
+                                        )
+                                }
+                        )
+                ),
                 @RouterOperation(
                         method = RequestMethod.GET,
                         path = "/current",
