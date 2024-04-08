09feefa55d62b4aad59ebaa30d72ab79a68a7eb5
George <george.niculae20@yahoo.com>
2024-04-08 16:42:28 +0300

    Update

--- swift-wheels-hub-reactive-customer/src/main/java/com/swiftwheelshubreactive/customer/SwiftWheelsHubReactiveCustomerApplication.java
+++ swift-wheels-hub-reactive-customer/src/main/java/com/swiftwheelshubreactive/customer/SwiftWheelsHubReactiveCustomerApplication.java
@@ -2,13 +2,13 @@ package com.swiftwheelshubreactive.customer;
 
 import com.swiftwheelshubreactive.lib.annotation.SwiftWheelsHubReactiveMicroservice;
 import org.springframework.boot.SpringApplication;
-import reactor.blockhound.BlockHound;
+//import reactor.blockhound.BlockHound;
 
 @SwiftWheelsHubReactiveMicroservice
 public class SwiftWheelsHubReactiveCustomerApplication {
 
     public static void main(String[] args) {
-        BlockHound.install(builder -> builder.allowBlockingCallsInside("io.netty.util.concurrent.FastThreadLocalRunnable", "run"));
+//        BlockHound.install(builder -> builder.allowBlockingCallsInside("io.netty.util.concurrent.FastThreadLocalRunnable", "run"));
         SpringApplication.run(SwiftWheelsHubReactiveCustomerApplication.class, args);
     }
 
--- swift-wheels-hub-reactive-email-notification/src/main/java/com/swiftwheelshubreactive/emailnotification/SwiftWheelsHubReactiveEmailNotificationApplication.java
+++ swift-wheels-hub-reactive-email-notification/src/main/java/com/swiftwheelshubreactive/emailnotification/SwiftWheelsHubReactiveEmailNotificationApplication.java
@@ -2,13 +2,13 @@ package com.swiftwheelshubreactive.emailnotification;
 
 import com.swiftwheelshubreactive.lib.annotation.SwiftWheelsHubReactiveMicroservice;
 import org.springframework.boot.SpringApplication;
-import reactor.blockhound.BlockHound;
+//import reactor.blockhound.BlockHound;
 
 @SwiftWheelsHubReactiveMicroservice
 public class SwiftWheelsHubReactiveEmailNotificationApplication {
 
     public static void main(String[] args) {
-        BlockHound.install();
+//        BlockHound.install();
         SpringApplication.run(SwiftWheelsHubReactiveEmailNotificationApplication.class);
     }
 
