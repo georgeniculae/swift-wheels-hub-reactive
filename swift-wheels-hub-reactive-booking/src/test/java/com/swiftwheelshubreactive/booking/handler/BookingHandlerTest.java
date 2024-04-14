331fc35d6dfab5e3edd2489287b267e4670ecab3
George <george.niculae20@yahoo.com>
2024-04-14 20:00:08 +0300

    Update

--- swift-wheels-hub-reactive-booking/src/test/java/com/swiftwheelshubreactive/booking/handler/BookingHandlerTest.java
+++ swift-wheels-hub-reactive-booking/src/test/java/com/swiftwheelshubreactive/booking/handler/BookingHandlerTest.java
@@ -20,9 +20,7 @@ import reactor.test.StepVerifier;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
