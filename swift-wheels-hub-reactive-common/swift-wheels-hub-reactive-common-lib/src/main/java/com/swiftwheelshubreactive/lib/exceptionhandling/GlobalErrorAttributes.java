c91c9d008c06f9387427eea725a784fcd0bc7c41
George <george.niculae20@yahoo.com>
2024-04-17 20:22:23 +0300

    Update

--- swift-wheels-hub-reactive-common/swift-wheels-hub-reactive-common-lib/src/main/java/com/swiftwheelshubreactive/lib/exceptionhandling/GlobalErrorAttributes.java
+++ swift-wheels-hub-reactive-common/swift-wheels-hub-reactive-common-lib/src/main/java/com/swiftwheelshubreactive/lib/exceptionhandling/GlobalErrorAttributes.java
@@ -1,11 +1,11 @@
 package com.swiftwheelshubreactive.lib.exceptionhandling;
 
-import com.swiftwheelshubreactive.exception.SwiftWheelsHubResponseStatusException;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.reactive.error.DefaultErrorAttributes;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Map;
 
@@ -39,8 +39,8 @@ public class GlobalErrorAttributes extends DefaultErrorAttributes {
             return UNEXPECTED_ERROR;
         }
 
-        if (error instanceof SwiftWheelsHubResponseStatusException swiftWheelsHubResponseStatusException) {
-            return swiftWheelsHubResponseStatusException.getReason();
+        if (error instanceof ResponseStatusException responseStatusException) {
+            return responseStatusException.getReason();
         }
 
         return message;
