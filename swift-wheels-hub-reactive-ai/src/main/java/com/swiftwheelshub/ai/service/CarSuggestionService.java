1d9b2fc28cbf9bfd7c2be87e49cc6df8a7bf5889
George <george.niculae20@yahoo.com>
2024-06-08 01:47:39 +0300

    Update

--- swift-wheels-hub-reactive-ai/src/main/java/com/swiftwheelshub/ai/service/CarSuggestionService.java
+++ swift-wheels-hub-reactive-ai/src/main/java/com/swiftwheelshub/ai/service/CarSuggestionService.java
@@ -45,7 +45,7 @@ public class CarSuggestionService {
     private String createChatPrompt(TripInfo tripInfo, List<String> cars) {
         return String.format(
                 """
-                        Which car from the following list %s is more suitable for rental from a car rental agency for
+                        Which car from the following list %s is more suitable for rental from a rental car agency for
                         a trip for %s people to %s, Romania in %s? The car will be used for %s.""",
                 cars,
                 tripInfo.peopleCount(),
