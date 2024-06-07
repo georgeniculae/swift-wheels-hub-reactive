faa136d5e913e904797f33fca8ce1777cddd969c
George <george.niculae20@yahoo.com>
2024-06-08 01:11:07 +0300

    Update

--- swift-wheels-hub-reactive-common/swift-wheels-hub-reactive-common-lib/src/main/java/com/swiftwheelshubreactive/lib/config/webclient/WebClientConfig.java
+++ swift-wheels-hub-reactive-common/swift-wheels-hub-reactive-common-lib/src/main/java/com/swiftwheelshubreactive/lib/config/webclient/WebClientConfig.java
@@ -1,13 +1,11 @@
 package com.swiftwheelshubreactive.lib.config.webclient;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-@RequiredArgsConstructor
 public class WebClientConfig {
 
     @Bean
