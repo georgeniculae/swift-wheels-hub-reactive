06e710efab2e64874e23823c09c61e0fdd239912
George <george.niculae20@yahoo.com>
2024-08-12 23:29:25 +0300

    Update

--- swift-wheels-hub-reactive-request-validator/src/main/java/com/swiftwheelshubreactive/requestvalidator/config/RegisteredEndpoints.java
+++ swift-wheels-hub-reactive-request-validator/src/main/java/com/swiftwheelshubreactive/requestvalidator/config/RegisteredEndpoints.java
@@ -1,7 +1,6 @@
 package com.swiftwheelshubreactive.requestvalidator.config;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +9,6 @@ import java.util.Map;
 @Component
 @ConfigurationProperties(prefix = "swagger")
 @Getter
-@Setter
 public class RegisteredEndpoints {
 
     private Map<String, String> endpoints;
