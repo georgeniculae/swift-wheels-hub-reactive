9d4bbcacfe85ce2b958f65a9b4649ec90aa99a80
George <george.niculae20@yahoo.com>
2024-08-16 22:15:59 +0300

    Update

---
+++ swift-wheels-hub-reactive-agency/src/main/java/com/swiftwheelshubreactive/agency/config/MongoProperties.java
@@ -0,0 +1,19 @@
+package com.swiftwheelshubreactive.agency.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("!test")
+@Getter
+public class MongoProperties {
+
+    @Value("${migration.packageScan}")
+    private String packageScan;
+
+    @Value("${spring.data.mongodb.database}")
+    private String databaseName;
+
+}
--- swift-wheels-hub-reactive-agency/src/main/java/com/swiftwheelshubreactive/agency/migration/MongoMigrationConfig.java
+++ swift-wheels-hub-reactive-agency/src/main/java/com/swiftwheelshubreactive/agency/migration/MongoMigrationConfig.java
@@ -1,10 +1,10 @@
 package com.swiftwheelshubreactive.agency.migration;
 
 import com.mongodb.reactivestreams.client.MongoClient;
+import com.swiftwheelshubreactive.agency.config.MongoProperties;
 import io.mongock.driver.mongodb.reactive.driver.MongoReactiveDriver;
 import io.mongock.runner.springboot.MongockSpringboot;
 import io.mongock.runner.springboot.base.MongockInitializingBeanRunner;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,13 +15,12 @@ import org.springframework.context.annotation.Profile;
 public class MongoMigrationConfig {
 
     @Bean
-    public MongockInitializingBeanRunner getBuilder(@Value("${migration.packageScan}") String packageScan,
-                                                    @Value("${spring.data.mongodb.database}") String databaseName,
+    public MongockInitializingBeanRunner getBuilder(MongoProperties mongoProperties,
                                                     MongoClient mongoClient,
                                                     ApplicationContext context) {
         return MongockSpringboot.builder()
-                .setDriver(MongoReactiveDriver.withDefaultLock(mongoClient, databaseName))
-                .addMigrationScanPackage(packageScan)
+                .setDriver(MongoReactiveDriver.withDefaultLock(mongoClient, mongoProperties.getDatabaseName()))
+                .addMigrationScanPackage(mongoProperties.getPackageScan())
                 .setSpringContext(context)
                 .buildInitializingBeanRunner();
     }
