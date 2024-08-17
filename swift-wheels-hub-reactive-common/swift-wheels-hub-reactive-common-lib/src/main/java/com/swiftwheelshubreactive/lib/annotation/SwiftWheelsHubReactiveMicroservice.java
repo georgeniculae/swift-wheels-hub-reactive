f65f199bc53f4a473e03e1ff5064672b39c52d02
George <george.niculae20@yahoo.com>
2024-08-18 00:24:52 +0300

    Update

--- swift-wheels-hub-reactive-common/swift-wheels-hub-reactive-common-lib/src/main/java/com/swiftwheelshubreactive/lib/annotation/SwiftWheelsHubReactiveMicroservice.java
+++ swift-wheels-hub-reactive-common/swift-wheels-hub-reactive-common-lib/src/main/java/com/swiftwheelshubreactive/lib/annotation/SwiftWheelsHubReactiveMicroservice.java
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
@@ -30,6 +29,5 @@ import java.lang.annotation.Target;
 @EnableDiscoveryClient
 @EnableScheduling
 @EnableConfigurationProperties
-@EnableCaching
 public @interface SwiftWheelsHubReactiveMicroservice {
 }
