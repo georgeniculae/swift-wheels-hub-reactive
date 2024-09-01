459eb64889c818564c362c1bfec52625656a5810
George <george.niculae20@yahoo.com>
2024-09-01 15:40:24 +0300

    Update

--- swift-wheels-hub-reactive-common/swift-wheels-hub-reactive-common-lib/src/main/java/com/swiftwheelshubreactive/lib/config/mongodb/ReactiveMongoTransactionManagerConfig.java
+++ swift-wheels-hub-reactive-common/swift-wheels-hub-reactive-common-lib/src/main/java/com/swiftwheelshubreactive/lib/config/mongodb/ReactiveMongoTransactionManagerConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
 import org.springframework.data.mongodb.ReactiveMongoTransactionManager;
+import org.springframework.transaction.ReactiveTransactionManager;
+import org.springframework.transaction.reactive.TransactionalOperator;
 
 @Configuration
 @ConditionalOnProperty(prefix = "mongo-operations", name = "enabled", havingValue = "true")
@@ -13,6 +15,11 @@ public class ReactiveMongoTransactionManagerConfig {
     @Bean
     public ReactiveMongoTransactionManager reactiveMongoTransactionManager(ReactiveMongoDatabaseFactory reactiveMongoDatabaseFactory) {
         return new ReactiveMongoTransactionManager(reactiveMongoDatabaseFactory);
+    }
+
+    @Bean
+    public TransactionalOperator transactionalOperator(ReactiveTransactionManager reactiveTransactionManager) {
+        return TransactionalOperator.create(reactiveTransactionManager);
     }
 
 }
