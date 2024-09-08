cf4d43f09ceb3989f099c3200581bc96d066c863
George <george.niculae20@yahoo.com>
2024-09-08 19:19:38 +0300

    Update

--- swift-wheels-hub-reactive-common/swift-wheels-hub-reactive-common-lib/src/main/java/com/swiftwheelshubreactive/lib/config/webclient/WebClientConfig.java
+++ swift-wheels-hub-reactive-common/swift-wheels-hub-reactive-common-lib/src/main/java/com/swiftwheelshubreactive/lib/config/webclient/WebClientConfig.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
@@ -17,7 +21,10 @@ public class WebClientConfig {
 
     @Bean
     public WebClient webClient(@Qualifier("loadBalancedWebClientBuilder") WebClient.Builder webClientBuilder) {
-        return webClientBuilder.build();
+        HttpClient httpClient = HttpClient.create().responseTimeout(Duration.ofSeconds(20));
+
+        return webClientBuilder.clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
     }
 
 }
