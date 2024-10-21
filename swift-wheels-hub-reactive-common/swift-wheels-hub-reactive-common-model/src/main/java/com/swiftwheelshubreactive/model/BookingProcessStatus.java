283343166c2bad7124a633013c4303673ce33a52
George <george.niculae20@yahoo.com>
2024-10-21 20:23:25 +0300

    Update

--- swift-wheels-hub-reactive-common/swift-wheels-hub-reactive-common-model/src/main/java/com/swiftwheelshubreactive/model/BookingProcessStatus.java
+++ swift-wheels-hub-reactive-common/swift-wheels-hub-reactive-common-model/src/main/java/com/swiftwheelshubreactive/model/BookingProcessStatus.java
@@ -2,11 +2,14 @@ package com.swiftwheelshubreactive.model;
 
 public enum BookingProcessStatus {
 
-    IN_CREATION,
-    IN_UPDATE,
-    IN_CLOSING,
+    CREATING,
+    UPDATING,
+    CLOSING,
     SAVED_CREATED_BOOKING,
     SAVED_UPDATED_BOOKING,
-    SAVED_CLOSED_BOOKING
+    SAVED_CLOSED_BOOKING,
+    FAILED_CREATED_BOOKING,
+    FAILED_UPDATED_BOOKING,
+    FAILED_CLOSED_BOOKING
 
 }
