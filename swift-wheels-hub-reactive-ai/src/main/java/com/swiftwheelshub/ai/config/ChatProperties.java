5105d99416b73aa003b79a48670b3c83124d380d
George <george.niculae20@yahoo.com>
2024-10-26 01:49:11 +0300

    Update

--- swift-wheels-hub-reactive-ai/src/main/java/com/swiftwheelshub/ai/config/ChatProperties.java
+++ swift-wheels-hub-reactive-ai/src/main/java/com/swiftwheelshub/ai/config/ChatProperties.java
@@ -12,6 +12,6 @@ import org.springframework.context.annotation.Configuration;
 public class ChatProperties {
 
     private String model;
-    private float temperature;
+    private Double temperature;
 
 }
