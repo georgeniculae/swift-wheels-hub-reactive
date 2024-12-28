70c6f3808bce3c6a7e1c8ee2e5e0d9175d7d7766
George <george.niculae20@yahoo.com>
2024-12-28 23:58:39 +0200

    Update

--- swift-wheels-hub-reactive-expense/src/test/java/com/swiftwheelshubreactive/expense/mapper/InvoiceMapperTest.java
+++ swift-wheels-hub-reactive-expense/src/test/java/com/swiftwheelshubreactive/expense/mapper/InvoiceMapperTest.java
@@ -1,15 +1,21 @@
 package com.swiftwheelshubreactive.expense.mapper;
 
+import com.swiftwheelshubreactive.dto.InvoiceProcessState;
 import com.swiftwheelshubreactive.dto.InvoiceRequest;
 import com.swiftwheelshubreactive.dto.InvoiceResponse;
+import com.swiftwheelshubreactive.exception.SwiftWheelsHubException;
 import com.swiftwheelshubreactive.expense.util.AssertionUtil;
 import com.swiftwheelshubreactive.expense.util.TestUtil;
 import com.swiftwheelshubreactive.model.Invoice;
+import com.swiftwheelshubreactive.model.InvoiceProcessStatus;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
 class InvoiceMapperTest {
@@ -35,6 +41,37 @@ class InvoiceMapperTest {
 
         assertNotNull(invoice);
         AssertionUtil.assertInvoiceRequest(invoice, invoiceRequest);
+    }
+
+    @Test
+    void mapToInvoiceProcessStateTest_null() {
+        assertNull(invoiceMapper.mapToInvoiceProcessState(null));
+    }
+
+    @Test
+    void mapToInvoiceProcessStateTest_savedClosedInvoice() {
+        InvoiceProcessState invoiceProcessState =
+                invoiceMapper.mapToInvoiceProcessState(InvoiceProcessStatus.SAVED_CLOSED_INVOICE);
+
+        assertEquals(InvoiceProcessState.SAVED_CLOSED_INVOICE.name(), invoiceProcessState.name());
+    }
+
+    @Test
+    void mapToInvoiceProcessStateTest_failedClosedInvoice() {
+        InvoiceProcessState invoiceProcessState =
+                invoiceMapper.mapToInvoiceProcessState(InvoiceProcessStatus.FAILED_CLOSED_INVOICE);
+
+        assertEquals(InvoiceProcessState.FAILED_CLOSED_INVOICE.name(), invoiceProcessState.name());
+    }
+
+    @Test
+    void mapToInvoiceProcessStateTest_defaultCase() {
+        SwiftWheelsHubException swiftWheelsHubException = assertThrows(
+                SwiftWheelsHubException.class,
+                () -> invoiceMapper.mapToInvoiceProcessState(InvoiceProcessStatus.IN_PROGRESS)
+        );
+
+        assertEquals("Invalid invoice process status: IN_PROGRESS", swiftWheelsHubException.getMessage());
     }
 
 }
