4aa0ac4e9c554e82fcee8162df9e41da4d0097f4
George <george.niculae20@yahoo.com>
2025-02-23 14:10:04 +0200

    Update

--- swift-wheels-hub-reactive-common/swift-wheels-hub-reactive-common-lib/src/main/java/com/swiftwheelshubreactive/lib/config/webclient/WebClientConfig.java
+++ swift-wheels-hub-reactive-common/swift-wheels-hub-reactive-common-lib/src/main/java/com/swiftwheelshubreactive/lib/config/webclient/WebClientConfig.java
@@ -1,6 +1,9 @@
 package com.swiftwheelshubreactive.lib.config.webclient;
 
 import io.netty.channel.ChannelOption;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 import java.time.Duration;
 
@@ -19,7 +23,11 @@ public class WebClientConfig {
     public WebClient.Builder webClientBuilder() {
         HttpClient httpClient = HttpClient.create()
                 .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 60000)
-                .responseTimeout(Duration.ofSeconds(60));
+                .responseTimeout(Duration.ofSeconds(60))
+                .doOnConnected(connection -> connection
+                        .addHandlerLast(new ReadTimeoutHandler(60))
+                        .addHandlerLast(new WriteTimeoutHandler(60)))
+                .wiretap("reactor.netty.http.client.HttpClient", LogLevel.INFO, AdvancedByteBufFormat.TEXTUAL);
 
         return WebClient.builder()
                 .clientConnector(new ReactorClientHttpConnector(httpClient));
