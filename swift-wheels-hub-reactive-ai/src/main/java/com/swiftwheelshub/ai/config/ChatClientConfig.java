ff0704f084f4c93f1390f4b69d010af3cc1bf252
George Niculae <george.niculae20@yahoo.com>
2025-03-29 19:32:13 +0200

    Update

--- swift-wheels-hub-reactive-ai/src/main/java/com/swiftwheelshub/ai/config/ChatClientConfig.java
+++ swift-wheels-hub-reactive-ai/src/main/java/com/swiftwheelshub/ai/config/ChatClientConfig.java
@@ -9,7 +9,11 @@ public class ChatClientConfig {
 
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
-        return chatClientBuilder.build();
+        return chatClientBuilder.defaultSystem("""
+                        You are a helpful assistant who can clearly and concisely answer questions about the type
+                        of vehicle that is most suitable for traveling to a certain location in Romania in a certain
+                        month of the year. You must provide one suggestion only.""")
+                .build();
     }
 
 }
