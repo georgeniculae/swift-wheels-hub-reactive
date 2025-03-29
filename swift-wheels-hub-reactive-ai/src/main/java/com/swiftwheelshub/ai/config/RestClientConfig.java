a9635596674879854a15702bd75f088059cf9349
George Niculae <george.niculae20@yahoo.com>
2025-03-29 19:11:03 +0200

    Update

---
+++ swift-wheels-hub-reactive-ai/src/main/java/com/swiftwheelshub/ai/config/RestClientConfig.java
@@ -0,0 +1,34 @@
+package com.swiftwheelshub.ai.config;
+
+import org.springframework.boot.http.client.ClientHttpRequestFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
+import java.util.List;
+
+@Configuration
+public class RestClientConfig {
+
+    @Bean
+    public RestClient.Builder restClientBuilder() {
+        SimpleClientHttpRequestFactory clientHttpRequestFactory = ClientHttpRequestFactoryBuilder.simple()
+                .withCustomizers(
+                        List.of(
+                                requestFactory -> requestFactory.setConnectTimeout(Duration.ofSeconds(60)),
+                                requestFactory -> requestFactory.setReadTimeout(Duration.ofSeconds(60))
+                        )
+                )
+                .build();
+
+        return RestClient.builder().requestFactory(clientHttpRequestFactory);
+    }
+
+    @Bean
+    public RestClient restClient(RestClient.Builder restClientBuilder) {
+        return restClientBuilder.build();
+    }
+
+}
