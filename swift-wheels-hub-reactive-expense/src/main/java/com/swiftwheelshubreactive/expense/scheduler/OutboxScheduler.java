9d4eed60f2ab2f5323da84f74c6378516cb61313
George Niculae <george.niculae20@yahoo.com>
2025-04-16 00:04:24 +0300

    Update

--- swift-wheels-hub-reactive-expense/src/main/java/com/swiftwheelshubreactive/expense/scheduler/OutboxScheduler.java
+++ swift-wheels-hub-reactive-expense/src/main/java/com/swiftwheelshubreactive/expense/scheduler/OutboxScheduler.java
@@ -2,6 +2,7 @@ package com.swiftwheelshubreactive.expense.scheduler;
 
 import com.swiftwheelshubreactive.expense.service.OutboxService;
 import lombok.RequiredArgsConstructor;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,11 @@ public class OutboxScheduler {
     private final OutboxService outboxService;
 
     @Scheduled(fixedDelayString = "${scheduled.fixedDelay}")
+    @SchedulerLock(
+            name = "invoiceSchedulerScheduledTask",
+            lockAtLeastFor = "30s",
+            lockAtMostFor = "1m"
+    )
     public void pollOutboxCollection() {
         outboxService.handleOutboxes()
                 .subscribe();
