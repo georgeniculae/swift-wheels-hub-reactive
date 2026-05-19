a777cf1e92435bb44c0e54e07188c4a4ec7dc8b3
George Niculae <george.niculae20@yahoo.com>
2026-05-19 22:15:48 +0300

    Update

--- auto-hub-reactive-invoice-notification/src/main/java/com/autohubreactive/invoicenotification/router/InvoiceDocumentRouter.java
+++ auto-hub-reactive-invoice-notification/src/main/java/com/autohubreactive/invoicenotification/router/InvoiceDocumentRouter.java
@@ -1,6 +1,7 @@
 package com.autohubreactive.invoicenotification.router;
 
 import com.autohubreactive.invoicenotification.handler.InvoiceDocumentHandler;
+import com.autohubreactive.invoicenotification.swaggeroperation.SwaggerInvoiceNotificationRouterOperations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -13,6 +14,7 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 public class InvoiceDocumentRouter {
 
     @Bean
+    @SwaggerInvoiceNotificationRouterOperations
     public RouterFunction<ServerResponse> routeInvoiceDocument(InvoiceDocumentHandler invoiceDocumentHandler) {
         return RouterFunctions.nest(
                 RequestPredicates.path("/invoices").and(RequestPredicates.accept(MediaType.APPLICATION_PDF)),
---
+++ auto-hub-reactive-invoice-notification/src/main/java/com/autohubreactive/invoicenotification/swaggeroperation/SwaggerInvoiceNotificationRouterOperations.java
@@ -0,0 +1,63 @@
+package com.autohubreactive.invoicenotification.swaggeroperation;
+
+import com.autohubreactive.invoicenotification.handler.InvoiceDocumentHandler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springdoc.core.annotations.RouterOperation;
+import org.springdoc.core.annotations.RouterOperations;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@RouterOperations(
+        {
+                @RouterOperation(
+                        method = RequestMethod.GET,
+                        path = "/invoices/{invoiceId}/document",
+                        beanClass = InvoiceDocumentHandler.class,
+                        beanMethod = "getInvoiceDocument",
+                        operation = @Operation(
+                                operationId = "getInvoiceDocument",
+                                responses = {
+                                        @ApiResponse(
+                                                responseCode = "200",
+                                                description = "Successful",
+                                                content = @Content(
+                                                        array = @ArraySchema(schema = @Schema(implementation = byte[].class)),
+                                                        mediaType = MediaType.APPLICATION_PDF_VALUE
+                                                )
+                                        ),
+                                        @ApiResponse(
+                                                responseCode = "400",
+                                                description = "Bad Request",
+                                                content = @Content(schema = @Schema())
+                                        ),
+                                        @ApiResponse(
+                                                responseCode = "500",
+                                                description = "Internal Server Error",
+                                                content = @Content(schema = @Schema())
+                                        )
+                                },
+                                parameters = @Parameter(
+                                        name = "invoiceId",
+                                        in = ParameterIn.PATH,
+                                        required = true,
+                                        content = @Content(schema = @Schema(implementation = String.class))
+                                )
+                        )
+                )
+        }
+)
+public @interface SwaggerInvoiceNotificationRouterOperations {
+}
